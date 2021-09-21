6ccaf7cbce82579f94a232d02b89d6edd27e4e00
Chathura Jayasankha <jayasankha1010@gmail.com>
2021-09-21 18:32:00 +0530

    Plot typical ECG signal

--- 1-Smoothing_filters/Moving_average_filters.m
+++ 1-Smoothing_filters/Moving_average_filters.m
@@ -11,8 +11,26 @@
 % A MA(N) filter can be visualised as a moving window across the signal. A point 
 % of the filtered signal ?(?) is derived from the average of the windowed points 
 % on the input signal ?(?).
-% Preliminaries
 
-% Load ECG_template.mat acquisition parameters of the signal
-%replace the following path to ad
-raw_data = load('D:\Semester 7\2. Biosignal Processing-3\Assignments\Biosignal-Digital-Filters\Data\ECG_template.mat');
+
+%% Preliminaries 
+%% Load ECG_template.mat acquisition parameters of the signal
+% replace the following path to ad
+clear all;
+close all;
+typical_ECG = load('D:\Semester 7\2. Biosignal Processing-3\Assignments\Biosignal-Digital-Filters\Data\ECG_template.mat');
+ecg_template = typical_ECG.ECG_template;
+%% Plot the loaded signal with the adjusted time scale
+fs = 500; %sampling frequency
+[~,N] = size(ecg_template); %Number of datapoints
+T = linspace(0,N/fs,N); %Time scale
+
+%plot the typical ECG Signal template
+figure('Name','Raw Data')
+plot(T,ecg_template)
+title('Typical ECG Signal')
+xlabel('Time (s)')
+ylabel('mV')
+
+%% 
+
